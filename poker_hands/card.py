fc748dce70515908e46dc9d60adc7bb21370eb63
Tomas Tamantini <tomasbft@gmail.com>
2021-05-28 16:39:57 -0300

    Feat: Implement card initialization

---
+++ poker_hands/card.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Suit(str, Enum):
+    HEARTS = 'h'
+    CLUBS = 'c'
+    DIAMONDS = 'd'
+    SPADES = 's'
+
+
+class FaceRank(int, Enum):
+    JACK = 11
+    QUEEN = 12
+    KING = 13
+    ACE = 14
+
+
+@dataclass
+class Card:
+    rank: int
+    suit: Suit
+
+    def __post_init__(self):
+        if self.rank < 1 or self.rank > 14:
+            raise ValueError('Invalid rank (must be a number between 1 and 14)')
