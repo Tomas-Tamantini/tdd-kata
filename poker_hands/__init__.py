63307b8fe03de5a33873ed90d11b440f74b696fe
Tomas Tamantini <tomasbft@gmail.com>
2021-05-28 19:38:30 -0300

    Feat: Create parse_hand method

--- poker_hands/__init__.py
+++ poker_hands/__init__.py
@@ -1,3 +1,3 @@
 from .card import Card, FaceRank, Suit
 from .hand import Hand
-from .parser import parse_card
+from .parser import parse_card, parse_hand
--- poker_hands/parser.py
+++ poker_hands/parser.py
@@ -1,10 +1,17 @@
-from poker_hands import Card, Suit
+from poker_hands import Card, Suit, Hand
 from poker_hands.card import RANK_REPR
 
 
 def parse_card(str_format: str) -> Card:
+    """Load card from strings of the form: 2h, 7s, Tc, Jd, Ah..."""
     stripped = str_format.strip()
     return Card(rank=_parse_rank(stripped[0]), suit=Suit(stripped[1].lower()))
+
+
+def parse_hand(str_format: str) -> Hand:
+    """Load hand from strings. The cards should be separated by comma such as in example: 2h, 7s, Tc, Jd, Ah"""
+    card_strings = str_format.split(',')
+    return Hand(cards=[parse_card(s) for s in card_strings])
 
 
 def _parse_rank(c: chr) -> int:
