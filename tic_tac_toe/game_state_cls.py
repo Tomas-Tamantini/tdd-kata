bd19d8843417a1e7be2f4d601bcc0663f71c8ec3
Tomas Tamantini <tomasbft@gmail.com>
2021-09-12 12:54:14 -0300

    Reorder methods

--- tic_tac_toe/game_state_cls.py
+++ tic_tac_toe/game_state_cls.py
@@ -10,17 +10,12 @@ class TicTacToe:
         self.__crosses = crosses if crosses else []
         self.__index = self.__get_index()
 
-    def __get_positions_score(self, positions: List[Tuple[int, int]], reflection: bool, rotation: int) -> int:
-        score = 0
-        for i, j in positions:
-            new_i, new_j = self.__get_transformed_coordinates(i, j, reflection, rotation)
-            power = 8 - 3 * new_i - new_j
-            score += 3 ** power
-        return score
+    @property
+    def num_empty_cells(self) -> int:
+        return 9 - len(self.__naughts) - len(self.__crosses)
 
-    def __get_score(self, reflection: bool, rotation: int) -> int:
-        return self.__get_positions_score(self.__naughts, reflection, rotation) + 2 * self.__get_positions_score(
-            self.__crosses, reflection, rotation)
+    def __hash__(self):
+        return self.__index
 
     def __get_index(self):
         max_score = 0
@@ -31,12 +26,17 @@ class TicTacToe:
                     max_score = score
         return max_score
 
-    @property
-    def num_empty_cells(self) -> int:
-        return 9 - len(self.__naughts) - len(self.__crosses)
+    def __get_score(self, reflection: bool, rotation: int) -> int:
+        return self.__get_positions_score(self.__naughts, reflection, rotation) + 2 * self.__get_positions_score(
+            self.__crosses, reflection, rotation)
 
-    def __hash__(self):
-        return self.__index
+    def __get_positions_score(self, positions: List[Tuple[int, int]], reflection: bool, rotation: int) -> int:
+        score = 0
+        for i, j in positions:
+            new_i, new_j = self.__get_transformed_coordinates(i, j, reflection, rotation)
+            power = 8 - 3 * new_i - new_j
+            score += 3 ** power
+        return score
 
     @staticmethod
     def __get_transformed_coordinates(i: int, j: int, reflection: bool, rotation: int) -> Tuple[int, int]:
