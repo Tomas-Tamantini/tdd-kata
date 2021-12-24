6f633dc506e0d69c82ca9a0378e1cd3926f397a5
Tomas Tamantini <tomasbft@gmail.com>
2021-12-24 13:05:38 -0300

    Create Frame class

--- bowling_game/game.py
+++ bowling_game/game.py
@@ -1,34 +1,84 @@
 """See game rules and kata specification at: https://kata-log.rocks/bowling-game-kata"""
+from typing import List
+
+
+class _Frame:
+    def __init__(self):
+        self.__rolls = []
+
+    def add_roll(self, pins: int) -> None:
+        if pins < 0 or pins > self.num_standing_pins:
+            raise ValueError("Invalid number of pins")
+        self.__rolls.append(pins)
+
+    @property
+    def rolls(self) -> List[int]:
+        return self.__rolls
+
+    @property
+    def score(self) -> int:
+        return sum(self.__rolls)
+
+    @property
+    def num_standing_pins(self) -> int:
+        if len(self.__rolls) == 0:
+            return 10
+        if self.is_strike:
+            if len(self.__rolls) > 1 and self.__rolls[1] == 10:
+                return 10
+            return 20 - self.score
+        if self.is_spare:
+            return 20 - self.score
+        return 10 - self.score
+
+    @property
+    def is_strike(self) -> bool:
+        return len(self.__rolls) > 0 and self.__rolls[0] == 10
+
+    @property
+    def is_spare(self) -> bool:
+        if len(self.__rolls) < 2 or self.__rolls[0] == 10:
+            return False
+        return self.__rolls[0] + self.__rolls[1] == 10
+
+    def is_over(self) -> bool:
+        if len(self.__rolls) == 0:
+            return False
+        return len(self.__rolls) == 2 or self.__rolls[0] == 10
 
 
 class BowlingGame:
     def __init__(self) -> None:
-        self.__rolls = []
-        self.__num_standing_pins = 10
-        self.__is_start_of_frame = True
-        self.__spare_indices = set()
-        self.__strike_indices = set()
+        self.__frames = []
+
+    def __current_frame(self) -> _Frame:
+        if len(self.__frames) == 0 or (len(self.__frames) < 10 and self.__frames[-1].is_over()):
+            self.__frames.append(_Frame())
+        return self.__frames[-1]
+
+    def __subsequent_rolls(self, frame: _Frame) -> List[int]:
+        frame_index = self.__frames.index(frame)
+        rolls = []
+        for i in range(frame_index + 1, len(self.__frames)):
+            rolls += self.__frames[i].rolls
+        return rolls
 
     def roll(self, pins: int) -> None:
-        if pins < 0 or pins > self.__num_standing_pins:
-            raise ValueError("Invalid number of pins")
-        roll_index = len(self.__rolls)
-        if self.__is_start_of_frame:
-            if pins == 10:
-                self.__strike_indices.add(roll_index)
-            else:
-                self.__is_start_of_frame = False
-                self.__num_standing_pins -= pins
-        else:
-            self.__is_start_of_frame = True
-            self.__num_standing_pins = 10
-            if pins + self.__rolls[- 1] == 10:
-                self.__spare_indices.add(roll_index)
-        self.__rolls.append(pins)
+        frame = self.__current_frame()
+        frame.add_roll(pins)
 
     def score(self) -> int:
-        s = sum(self.__rolls)
-        for i in self.__spare_indices:
-            if i + 1 < len(self.__rolls):
-                s += self.__rolls[i+1]
+        s = 0
+        for i, frame in enumerate(self.__frames):
+            s += frame.score
+            if i + 1 >= len(self.__frames):
+                continue
+            rolls = self.__subsequent_rolls(frame)
+            if frame.is_spare and len(rolls) > 0:
+                s += rolls[0]
+            elif frame.is_strike:
+                if len(rolls) > 0:
+                    s += rolls[0]
+                if len(rolls) > 1:
+                    s += rolls[1]
         return s
