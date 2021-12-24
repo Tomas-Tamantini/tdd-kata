0491d123ef9d696bd97c3acbd80f916ee1864c93
Tomas Tamantini <tomasbft@gmail.com>
2021-12-24 11:40:04 -0300

    Test game with no spares or strikes

--- tests/test_bowling_game.py
+++ tests/test_bowling_game.py
@@ -1,8 +1,18 @@
 from bowling_game import BowlingGame
 
 
-def test_can_score_gutter_game():
+def roll_many(pins: int, times: int = 20) -> BowlingGame:
     game = BowlingGame()
-    for _ in range(20):
-        game.roll(0)
+    for _ in range(times):
+        game.roll(pins)
+    return game
+
+
+def test_can_score_gutter_game():
+    game = roll_many(0)
     assert game.score() == 0
+
+
+def test_can_score_all_ones():
+    game = roll_many(1)
+    assert game.score() == 20
