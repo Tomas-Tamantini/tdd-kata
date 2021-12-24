bf5b31abba2fa2ba3aa81587136a5c46c0a68169
Tomas Tamantini <tomasbft@gmail.com>
2021-12-24 11:37:12 -0300

    Start implementing public methods

--- bowling_game/game.py
+++ bowling_game/game.py
@@ -1,4 +1,9 @@
 """See game rules and kata specification at: https://kata-log.rocks/bowling-game-kata"""
 
+
 class BowlingGame:
-    pass
+    def roll(self, pins: int) -> None:
+        pass
+
+    def score(self) -> int:
+        return 0
