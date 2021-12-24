c130e007eeba27cbf321399d322b92fa7824eaa1
Tomas Tamantini <tomasbft@gmail.com>
2021-12-24 12:12:53 -0300

    Keep track of spares

--- bowling_game/game.py
+++ bowling_game/game.py
@@ -6,17 +6,29 @@ class BowlingGame:
         self.__rolls = []
         self.__num_standing_pins = 10
         self.__is_start_of_frame = True
+        self.__spare_indices = set()
+        self.__strike_indices = set()
 
     def roll(self, pins: int) -> None:
         if pins < 0 or pins > self.__num_standing_pins:
             raise ValueError("Invalid number of pins")
-        self.__rolls.append(pins)
-        if self.__is_start_of_frame and pins < 10:
-            self.__num_standing_pins -= pins
-            self.__is_start_of_frame = False
+        roll_index = len(self.__rolls)
+        if self.__is_start_of_frame:
+            if pins == 10:
+                self.__strike_indices.add(roll_index)
+            else:
+                self.__is_start_of_frame = False
+                self.__num_standing_pins -= pins
         else:
-            self.__num_standing_pins = 10
             self.__is_start_of_frame = True
+            self.__num_standing_pins = 10
+            if pins + self.__rolls[- 1] == 10:
+                self.__spare_indices.add(roll_index)
+        self.__rolls.append(pins)
 
     def score(self) -> int:
-        return sum(self.__rolls)
+        s = sum(self.__rolls)
+        for i in self.__spare_indices:
+            if i + 1 < len(self.__rolls):
+                s += self.__rolls[i+1]
+        return s
