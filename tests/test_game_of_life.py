a7617e6398668aa69f3ed6502aada9d0941258e8
Tomas Tamantini <tomasbft@gmail.com>
2021-12-24 10:33:21 -0300

    Test death by underpopulation

--- tests/test_game_of_life.py
+++ tests/test_game_of_life.py
@@ -1,4 +1,11 @@
+from typing import Optional, Set, Tuple
+
 from game_of_life import World
+
+
+def get_next_gen(live_cells: Optional[Set[Tuple[int, int]]] = None) -> World:
+    world = World(live_cells)
+    return world.next_generation()
 
 
 def test_default_world_is_empty():
@@ -12,6 +19,10 @@ def test_can_setup_world_with_live_cells():
 
 
 def test_dead_world_stays_dead():
-    dead_world = World()
-    second_gen = dead_world.next_generation()
+    second_gen = get_next_gen()
+    assert second_gen.num_live_cells == 0
+
+
+def test_death_by_underpopulation():
+    second_gen = get_next_gen({(1, 1)})
     assert second_gen.num_live_cells == 0
