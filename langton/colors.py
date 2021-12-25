d237d136c0e176a95284f40df22ffa61bfc24df5
Tomas Tamantini <tomasbft@gmail.com>
2021-12-25 18:26:12 -0300

    Implement get_next method

--- langton/colors.py
+++ langton/colors.py
@@ -5,3 +5,10 @@ class Color(Enum):
     WHITE = auto()
     BLACK = auto()
     RED = auto()
+
+    def get_next(current_color: 'Color', cycle_size: int) -> 'Color':
+        if current_color == Color.WHITE:
+            return Color.BLACK
+        if current_color == Color.RED:
+            return Color.WHITE
+        return Color.RED if cycle_size == 3 else Color.WHITE
