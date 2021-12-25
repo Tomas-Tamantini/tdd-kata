6535e55ae02f9edfb77263327ad4646e4a6332df
Tomas Tamantini <tomasbft@gmail.com>
2021-12-25 18:42:41 -0300

    Implement move_forward method

--- langton/ant.py
+++ langton/ant.py
@@ -18,3 +18,13 @@ class Ant:
 
     def turn(self, right: bool):
         self.__direction = Direction.get_next(self.__direction, right)
+
+    def move_forward(self):
+        if self.__direction == Direction.EAST:
+            self.__x += 1
+        elif self.__direction == Direction.NORTH:
+            self.__y += 1
+        elif self.__direction == Direction.WEST:
+            self.__x -= 1
+        elif self.__direction == Direction.SOUTH:
+            self.__y -= 1
