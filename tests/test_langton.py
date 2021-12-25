25cdbdc612ddfac730e361267de62a39efe7e9c6
Tomas Tamantini <tomasbft@gmail.com>
2021-12-25 17:54:51 -0300

    Test black to white transition

--- tests/test_langton.py
+++ tests/test_langton.py
@@ -3,6 +3,13 @@ from langton import Langton, Color
 
 def test_ant_turns_white_tile_to_black():
     langton = Langton(ant=(0, 0))
-    assert (0, 0) not in langton.tiles(Color.BLACK)
+    assert langton.get_color(0, 0) == Color.WHITE
     langton.tick()
-    assert (0, 0) in langton.tiles(Color.BLACK)
+    assert langton.get_color(0, 0) == Color.BLACK
+
+
+def test_ant_turns_black_tile_to_white_when_two_colors():
+    langton = Langton(ant=(0, 0), num_colors=2, tiles={Color.BLACK: {(0, 0)}})
+    assert langton.get_color(0, 0) == Color.BLACK
+    langton.tick()
+    assert langton.get_color(0, 0) == Color.WHITE
