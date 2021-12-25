1edbb8a46852516fcb1c97d4721940a03f0a4d49
Tomas Tamantini <tomasbft@gmail.com>
2021-12-25 18:26:38 -0300

    Use get_next method from Color class

--- langton/langton.py
+++ langton/langton.py
@@ -23,18 +23,14 @@ class Langton:
         return self.__ant.direction
 
     def tick(self) -> None:
-        colors = [Color.WHITE, Color.BLACK]
-        if self.__num_colors == 3:
-            colors.append(Color.RED)
-        for i, c in enumerate(colors):
-            if c not in self.__tiles or self.__ant.position not in self.__tiles[c]:
-                continue
-            self.__tiles[c].remove(self.__ant.position)
-            next_color = colors[(i + 1) % len(colors)]
-            if next_color != Color.WHITE:
-                self.__tiles[next_color].add(self.__ant.position)
-            return
-        self.__tiles[Color.BLACK].add(self.__ant.position)
+        p = self.__ant.position
+        current_color = self.get_color(*p)
+        next_color = Color.get_next(current_color, self.__num_colors)
+
+        if current_color in self.__tiles:
+            self.__tiles[current_color].remove(p)
+        if next_color in self.__tiles:
+            self.__tiles[next_color].add(p)
 
     def get_color(self, x: int, y: int) -> Color:
         for c in Color:
