7f26bfea1994663bf2fd939da27f6476004695f2
Tomas Tamantini <tomasbft@gmail.com>
2021-12-25 18:19:31 -0300

    Create Directions enum

--- langton/__init__.py
+++ langton/__init__.py
@@ -1,3 +1,4 @@
 from .colors import Color
+from .directions import Direction
 from .ant import Ant
 from .langton import Langton
---
+++ langton/directions.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+
+class Direction(Enum):
+    EAST = auto()
+    NORTH = auto()
+    WEST = auto()
+    SOUTH = auto()
