670af6ed4234d4d845eb0540402f9e2852081583
Tomas Tamantini <tomasbft@gmail.com>
2021-12-26 20:34:15 -0300

    Create class for methods

--- langton/langton_animation.py
+++ langton/langton_animation.py
@@ -1,78 +1,80 @@
-from tkinter import *
+from tkinter import Canvas
 from langton import Langton, Color, Direction
-from utils import run_animation
-
-WIDTH = 500
-HEIGHT = 500
-TILE_SIZE = 12
+from utils import run_animation, Grid
 
 
-def _setup_initial_grid(num_colors: int) -> Langton:
-    if num_colors == 2:
-        return Langton()
-    elif num_colors == 3:
+class LangtonAnimator:
+    def __init__(self, num_colors: int) -> None:
+        self.__grid = Grid(50, 40)  # 50 x 40 cells
+        self.__langton = self.__setup_initial_state(num_colors)
+
+    @property
+    def width(self) -> int:
+        return self.__grid.width
+
+    @property
+    def height(self) -> int:
+        return self.__grid.height
+
+    def __setup_initial_state(self, num_colors: int) -> Langton:
+        if num_colors < 2 or num_colors > 3:
+            raise ValueError('num_colors must be 2 or 3')
+        if num_colors == 2:
+            return Langton()
         return Langton(num_colors=3, tiles={
             Color.BLACK: {(0, 0), (5, 0), (-5, 0), (0, 15)},
             Color.RED: {(0, 5), (0, -5)}
         })
-    raise ValueError('num_colors must be 2 or 3')
+
+    def draw_world(self, canvas: Canvas):
+        self.__draw_tiles(canvas)
+        self.__draw_ant(canvas)
+        self.__langton.tick()
+
+    def __draw_tiles(self, canvas: Canvas):
+        for color, color_name in [(Color.BLACK, 'black'), (Color.RED, 'red')]:
+            tiles = self.__langton.tiles(color)
+            for t in tiles:
+                rect_coordinates = self.__grid.get_rectangle_coordinates(*t)
+                canvas.create_rectangle(
+                    *rect_coordinates, fill=color_name, outline='grey')
+
+    def __draw_ant(self, canvas: Canvas):
+        body_size = (self.__grid.tile_size * 3) // 5
+        head_size = body_size // 2
+        x, y = self.__langton.ant_position
+        x0, y0, *_ = self.__grid.get_rectangle_coordinates(x, y)
+        x0_body = x0 + (self.__grid.tile_size - body_size) // 2
+        y0_body = y0 + (self.__grid.tile_size - body_size) // 2
+        x0_head = x0 + (self.__grid.tile_size - head_size) // 2
+        y0_head = y0 + (self.__grid.tile_size - head_size) // 2
+        dir_offset = body_size // 3
+        if self.__langton.ant_direction == Direction.EAST:
+            x0_body -= dir_offset
+            x0_head += dir_offset
+        elif self.__langton.ant_direction == Direction.WEST:
+            x0_body += dir_offset
+            x0_head -= dir_offset
+        elif self.__langton.ant_direction == Direction.NORTH:
+            y0_body += dir_offset
+            y0_head -= dir_offset
+        elif self.__langton.ant_direction == Direction.SOUTH:
+            y0_body -= dir_offset
+            y0_head += dir_offset
+
+        x1_body = x0_body + body_size
+        y1_body = y0_body + body_size
+
+        x1_head = x0_head + head_size
+        y1_head = y0_head + head_size
+
+        canvas.create_oval(x0_body, y0_body, x1_body, y1_body,
+                           fill='blue', outline='black')
+        canvas.create_oval(x0_head, y0_head, x1_head, y1_head,
+                           fill='blue', outline='black')
 
 
 def run_langton_animation(num_colors: int = 3, num_iterations: int = 10000, frame_ms=100):
-    langton = _setup_initial_grid(num_colors)
-
-    def callback_on_frame(canvas: Canvas):
-        _draw_world(canvas, langton)
-        langton.tick()
-    run_animation(callback_on_frame, WIDTH, HEIGHT, num_iterations, frame_ms)
-
-
-def _draw_world(canvas: Canvas, langton: Langton):
-    _draw_tiles(canvas, langton)
-    _draw_ant(canvas, langton)
-
-
-def _draw_tiles(canvas: Canvas, langton: Langton):
-    for color, color_name in [(Color.BLACK, 'black'), (Color.RED, 'red')]:
-        tiles = langton.tiles(color)
-        for x, y in tiles:
-            x0 = x * TILE_SIZE + WIDTH // 2
-            y0 = -y * TILE_SIZE + HEIGHT // 2
-            x1 = x0 + TILE_SIZE
-            y1 = y0 + TILE_SIZE
-            canvas.create_rectangle(
-                x0, y0, x1, y1, fill=color_name, outline='grey')
-
-
-def _draw_ant(canvas: Canvas, langton: Langton):
-    body_size = (TILE_SIZE * 3) // 5
-    head_size = body_size // 2
-    x, y = langton.ant_position
-    x0_body = x * TILE_SIZE + (WIDTH + TILE_SIZE - body_size) // 2
-    y0_body = -y * TILE_SIZE + (HEIGHT + TILE_SIZE - body_size) // 2
-    x0_head = x * TILE_SIZE + (WIDTH + TILE_SIZE - head_size) // 2
-    y0_head = -y * TILE_SIZE + (HEIGHT + TILE_SIZE - head_size) // 2
-    dir_offset = body_size // 3
-    if langton.ant_direction == Direction.EAST:
-        x0_body -= dir_offset
-        x0_head += dir_offset
-    elif langton.ant_direction == Direction.WEST:
-        x0_body += dir_offset
-        x0_head -= dir_offset
-    elif langton.ant_direction == Direction.NORTH:
-        y0_body += dir_offset
-        y0_head -= dir_offset
-    elif langton.ant_direction == Direction.SOUTH:
-        y0_body -= dir_offset
-        y0_head += dir_offset
-
-    x1_body = x0_body + body_size
-    y1_body = y0_body + body_size
-
-    x1_head = x0_head + head_size
-    y1_head = y0_head + head_size
-
-    canvas.create_oval(x0_body, y0_body, x1_body, y1_body,
-                       fill='blue', outline='black')
-    canvas.create_oval(x0_head, y0_head, x1_head, y1_head,
-                       fill='blue', outline='black')
+    langton_animator = LangtonAnimator(num_colors)
+    run_animation(langton_animator.draw_world, langton_animator.width,
+                  langton_animator.height, num_iterations, frame_ms)
