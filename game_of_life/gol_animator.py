f02ae5091cf1fb82cd60927c22d8e472ff1d95a9
Tomas Tamantini <tomasbft@gmail.com>
2021-12-26 21:00:06 -0300

    Make a more interesting animation

--- game_of_life/gol_animator.py
+++ game_of_life/gol_animator.py
@@ -7,7 +7,7 @@ from game_of_life import World
 
 class _GameOfLifeAnimator:
     def __init__(self) -> None:
-        self.__grid = Grid(50, 40)  # 50 x 40 cells
+        self.__grid = Grid(100, 100, tile_size_in_pixels=5)  # 50 x 40 cells
         self.__world = self.__setup_initial_state()
 
     @property
@@ -19,7 +19,11 @@ class _GameOfLifeAnimator:
         return self.__grid.height
 
     def __setup_initial_state(self) -> World:
-        return World({(0, 0), (0, 1), (0, 2)})
+        cells_left = {
+            (0, 0), (0, 1), (0, 2), (1, 1), (2, 1), (3, 0), (3, 2)
+        }
+        cells_right = {(7 - x, y) for (x, y) in cells_left}
+        return World(cells_left.union(cells_right))
 
     def draw_world(self, canvas: Canvas):
         self.__draw_tiles(canvas)
@@ -31,7 +35,7 @@ class _GameOfLifeAnimator:
             canvas.create_rectangle(*rc, fill='black', outline='grey')
 
 
-def run_game_of_life_animation(num_iterations: int = 10000, frame_ms=250):
+def run_game_of_life_animation(num_iterations: int = 10000, frame_ms=100):
     animator = _GameOfLifeAnimator()
     run_animation(animator.draw_world, animator.width,
                   animator.height, num_iterations, frame_ms)
