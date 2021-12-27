4f835de298486901934027f098cc413148ffa2e6
Tomas Tamantini <tomasbft@gmail.com>
2021-12-26 21:02:23 -0300

    Run animation faster

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from langton import run_langton_animation
 from game_of_life import run_game_of_life_animation
 
 # run_langton_animation()
-run_game_of_life_animation()
+run_game_of_life_animation(frame_ms=25)
