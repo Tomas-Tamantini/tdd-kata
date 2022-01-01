3ed8ba4d85c2c6c8b1a62647a6fc7556756db459
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 13:22:25 -0300

    Test bomb outside of grid

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -9,7 +9,7 @@ def test_grid_starts_with_all_hidden_cells():
 @pytest.mark.parametrize('i, j', [(-1, 0), (0, -1), (10, 5), (5, 8)])
 def test_cannot_show_cell_not_on_grid(i, j):
     sweeper = MineSweeper(width=10, height=8)
-    with pytest.raises(ValueError):
+    with pytest.raises(IndexError):
         sweeper.click_cell(i, j)
 
 
@@ -17,3 +17,9 @@ def test_click_should_show_cell():
     sweeper = MineSweeper(10, 8)
     sweeper.click_cell(5, 5)
     assert sweeper.num_hidden_cells < 80
+
+
+def test_cannot_place_bombs_outside_grid():
+    sweeper = MineSweeper(10, 8)
+    with pytest.raises(IndexError):
+        sweeper.place_bombs({(5, 8)})
