e2b0219b662a31431e429dd0be71d4189be7f9dd
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 11:03:39 -0300

    List next ones to solve

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from langton import run_langton_animation
 from game_of_life import run_game_of_life_animation
+from langton import run_langton_animation
 
 # run_langton_animation()
 run_game_of_life_animation(frame_ms=25)
+
+# Next ones:
+# Mine sweeper
+# https://kata-log.rocks/ohce-kata
+# https://kata-log.rocks/task-list-kata
+# https://kata-log.rocks/unusual-spending-kata
+# https://codingdojo.org/kata/Potter/
+# https://codingdojo.org/kata/QotdCgi/
+# https://codingdojo.org/kata/Reversi/
+# https://codingdojo.org/kata/RomanCalculator/
+# https://codingdojo.org/kata/Tennis/
