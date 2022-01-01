090f6f60d0b5d2c6892d08ff42c0daabd7e06e78
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 13:30:11 -0300

    Set cells as bombs

--- minesweeper/sweeper.py
+++ minesweeper/sweeper.py
@@ -1,13 +1,21 @@
-from typing import Set, Tuple
+from typing import List, Set, Tuple
 
 
 class _Cell:
     def __init__(self) -> None:
         self.__is_hidden = True
+        self.__is_bomb = False
 
     @property
     def is_hidden(self) -> bool:
         return self.__is_hidden
+
+    @property
+    def is_bomb(self) -> bool:
+        return self.__is_bomb
+
+    def set_as_bomb(self) -> None:
+        self.__is_bomb = True
 
     def click(self):
         self.__is_hidden = False
@@ -31,11 +39,18 @@ class MineSweeper:
         return 0 <= i < self.width and 0 <= j < self.height
 
     @property
+    def __flat_cells(self) -> List[_Cell]:
+        return [self.__cells[i][j]
+                for i in range(self.width)
+                for j in range(self.height)]
+
+    @property
     def num_hidden_cells(self) -> int:
-        flat_cells = [self.__cells[i][j]
-                      for i in range(self.width)
-                      for j in range(self.height)]
-        return len([1 for c in flat_cells if c.is_hidden])
+        return len([1 for c in self.__flat_cells if c.is_hidden])
+
+    @property
+    def num_bombs(self) -> int:
+        return len([1 for c in self.__flat_cells if c.is_bomb])
 
     def click_cell(self, i: int, j: int):
         if not self.__is_inside_grid(i, j):
@@ -43,5 +58,7 @@ class MineSweeper:
         self.__cells[i][j].click()
 
     def place_bombs(self, bombs: Set[Tuple[int, int]]) -> None:
-        if any([not self.__is_inside_grid(*b) for b in bombs]):
-            raise IndexError('Cannot place bomb outside of grid')
+        for i, j in bombs:
+            if not self.__is_inside_grid(i, j):
+                raise IndexError('Cannot place bomb outside of grid')
+            self.__cells[i][j].set_as_bomb()
