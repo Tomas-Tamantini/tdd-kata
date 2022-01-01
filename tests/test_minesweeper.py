a165bcbc73811a1c25bce277780ce3a0ed9bea1c
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 11:30:58 -0300

    Test that click shows hidden cell

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,5 +1,5 @@
 import pytest
-from minesweeper import MineSweeper
+from minesweeper import MineSweeper, sweeper
 
 
 def test_grid_starts_with_all_hidden_cells():
@@ -11,3 +11,9 @@ def test_cannot_show_cell_not_on_grid(i, j):
     sweeper = MineSweeper(width=10, height=8)
     with pytest.raises(ValueError):
         sweeper.click_cell(i, j)
+
+
+def test_click_should_show_cell():
+    sweeper = MineSweeper(10, 8)
+    sweeper.click_cell(5, 5)
+    assert sweeper.num_hidden_cells < 80
