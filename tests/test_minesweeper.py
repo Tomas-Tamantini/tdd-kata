43fb3f6e435f7fb555ab80d808a6f04520d1f226
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 11:13:06 -0300

    Test click outside of grid

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,5 +1,13 @@
+import pytest
 from minesweeper import MineSweeper
 
 
 def test_grid_starts_with_all_hidden_cells():
     assert MineSweeper(width=10, height=8).num_hidden_cells == 80
+
+
+@pytest.mark.parametrize('i, j', [(-1, 0), (0, -1), (10, 5), (5, 8)])
+def test_cannot_show_cell_not_on_grid(i, j):
+    sweeper = MineSweeper(width=10, height=8)
+    with pytest.raises(ValueError):
+        sweeper.click_cell(i, j)
