909411a339305c421409f1276723ee1ab717e7a1
Tomas Tamantini <tomasbft@gmail.com>
2022-01-01 11:12:54 -0300

    Start implementing click_cell logic

--- minesweeper/sweeper.py
+++ minesweeper/sweeper.py
@@ -6,3 +6,6 @@ class MineSweeper:
     @property
     def num_hidden_cells(self) -> int:
         return self.__width * self.__height
+
+    def click_cell(self, i: int, j: int):
+        raise ValueError('Cannot click outside of grid')
