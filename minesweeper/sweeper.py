6d92c77f3d5650913b8ef40d089332f050d5cef9
Tomas Tamantini <tomasbft@gmail.com>
2022-01-02 10:33:36 -0300

    Get number of neighboring bombs

--- minesweeper/sweeper.py
+++ minesweeper/sweeper.py
@@ -1,30 +1,38 @@
-from typing import List, Set, Tuple
+from typing import List, Optional, Set, Tuple
 
 from minesweeper.cell import Cell
 
 
 class MineSweeper:
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, height: int, width: int) -> None:
         self.__cells = [
-            [Cell() for _ in range(height)]
-            for __ in range(width)]
+            [Cell() for _ in range(width)]
+            for __ in range(height)]
 
     @property
     def width(self) -> int:
-        return len(self.__cells)
+        return len(self.__cells[0])
 
     @property
     def height(self) -> int:
-        return len(self.__cells[0])
+        return len(self.__cells)
+
+    @property
+    def num_neighboring_bombs(self) -> List[List[Optional[int]]]:
+        def get_num_neighbors(cell: Cell) -> Optional[int]:
+            if cell.is_bomb or cell.is_hidden:
+                return None
+            return cell.num_neighboring_bombs
+        return [[get_num_neighbors(cell) for cell in row] for row in self.__cells]
 
     def __is_inside_grid(self, i: int, j: int) -> bool:
-        return 0 <= i < self.width and 0 <= j < self.height
+        return 0 <= i < self.height and 0 <= j < self.width
 
     @property
     def __flat_cells(self) -> List[Cell]:
         return [self.__cells[i][j]
-                for i in range(self.width)
-                for j in range(self.height)]
+                for i in range(self.height)
+                for j in range(self.width)]
 
     @property
     def num_hidden_cells(self) -> int:
@@ -39,6 +47,16 @@ class MineSweeper:
             raise IndexError('Cannot click outside of grid')
         self.__cells[i][j].click()
 
+    def __neighbors(self, i: int, j: int) -> List[Cell]:
+        out = []
+        for di in range(-1, 2):
+            for dj in range(-1, 2):
+                if di == 0 and dj == 0:
+                    continue
+                if self.__is_inside_grid(i + di, j + dj):
+                    out.append(self.__cells[i + di][j + dj])
+        return out
+
     def place_bombs(self, bombs: Set[Tuple[int, int]]) -> None:
         if self.num_bombs > 0:
             raise OverflowError('Can only place bombs once')
@@ -46,3 +64,5 @@ class MineSweeper:
             if not self.__is_inside_grid(i, j):
                 raise IndexError('Cannot place bomb outside of grid')
             self.__cells[i][j].set_as_bomb()
+            for c in self.__neighbors(i, j):
+                c.increment_num_neighboring_bombs()
--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -1,14 +1,16 @@
+from typing import List, Optional
+
 import pytest
-from minesweeper import MineSweeper, sweeper
+from minesweeper import MineSweeper
 
 
 def test_grid_starts_with_all_hidden_cells():
     assert MineSweeper(width=10, height=8).num_hidden_cells == 80
 
 
-@pytest.mark.parametrize('i, j', [(-1, 0), (0, -1), (10, 5), (5, 8)])
+@pytest.mark.parametrize('i, j', [(-1, 0), (0, -1), (8, 5), (5, 10)])
 def test_cannot_show_cell_not_on_grid(i, j):
-    sweeper = MineSweeper(width=10, height=8)
+    sweeper = MineSweeper(height=8, width=10)
     with pytest.raises(IndexError):
         sweeper.click_cell(i, j)
 
@@ -37,3 +39,13 @@ def test_can_only_place_bombs_once():
     with pytest.raises(OverflowError):
         sweeper.place_bombs({(0, 0)})
 
+
+def test_get_num_neighboring_bombs():
+    sweeper = MineSweeper(width=4, height=3)
+    sweeper.place_bombs({(0, 0), (1, 2), (2, 2)})
+    expected_neighbors: List[List[Optional[int]]] = [
+        [None for _ in range(4)] for _ in range(3)]
+    assert sweeper.num_neighboring_bombs == expected_neighbors
+    sweeper.click_cell(1, 1)
+    expected_neighbors[1][1] = 3
+    assert sweeper.num_neighboring_bombs == expected_neighbors
